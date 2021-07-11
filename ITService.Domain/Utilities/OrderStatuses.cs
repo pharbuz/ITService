a4d7bbed99c9a83ddb60bf54dca93f7b429e5f94
Robert Glodek <youtube1924@o2.pl>
2021-07-12 00:56:37 +0200

    Next

---
+++ ITService.Domain/Utilities/OrderStatuses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITService.Domain.Utilities
+{
+    public static class OrderStatuses
+    {
+
+        public const string StatusPending = "Waiting";
+        public const string StatusApproved = "Approved"; 
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Waiting";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+
+    }
+}
