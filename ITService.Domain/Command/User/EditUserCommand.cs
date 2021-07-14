207f58718224ea777eb16411610b22c91a14df9f
Jan Drabek <twojemail@example.com>
2021-07-14 23:10:09 +0200

    Someone delete command so I delete test for it.

--- ITService.Domain/Command/User/EditUserCommand.cs
+++ ITService.Domain/Command/User/EditUserCommand.cs
@@ -12,6 +12,5 @@ namespace ITService.Domain.Command.User
         public string Street { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
-        public string PhoneNumber { get; set; }
     }
 }
